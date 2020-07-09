b2e10ee99c68d953e65f094594be756449a59464
takashi iyama <takkun.iyama@nifty.com>
2020-07-09 11:10:50 +0900

    "<"での判断を"<="での判断に変更

--- Salary.cs
+++ Salary.cs
@@ -13,26 +13,36 @@ namespace CalcCalaryIncome {
                 if (earning < 1)
                     throw new ArgumentException("Earning must Great Equal 1 Yen");
 
-                if (earning < 551000)
+                if (earning <= 550999)
                     return 0;
-                if (earning < 1619999)
+
+                if (earning <= 1618999)
                     return earning - 550000;
-                if (earning < 1620000)
+
+                if (earning <= 1619999)
                     return (int)Math.Floor(earning * 0.6m) + 97600;
-                if (earning < 1622000)
+
+                if (earning <= 1621999)
                     return (int)Math.Floor(earning * 0.6m) + 98000;
-                if (earning < 1624000)
+
+                if (earning <= 1623999)
                     return (int)Math.Floor(earning * 0.6m) + 98800;
-                if (earning < 1628000)
+
+                if (earning <= 1627999)
                     return (int)Math.Floor(earning * 0.6m) + 99600;
-                if (earning < 1800000)
+
+                if (earning <= 1799999)
                     return (int)Math.Floor(earning * 0.6m) + 100000;
-                if (earning < 3600000)
+
+                if (earning <= 3599999)
                     return (int)Math.Floor(earning * 0.7m) - 80000;
-                if (earning < 6600000)
+
+                if (earning <= 6599999)
                     return (int)Math.Floor(earning * 0.8m) - 440000;
-                if (earning < 8500000)
+
+                if (earning <= 8499999)
                     return (int)Math.Floor(earning * 0.9m) - 1100000;
+
                 if (earning <= 20000000)
                     return earning - 1950000;
 
